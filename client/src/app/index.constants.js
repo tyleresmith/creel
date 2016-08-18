c888dfded86b3e689b015f041ea18fb115504c9a
Tyler Smith <tyler.e.smith000@gmail.com>
2016-08-18 23:53:58 +0000

    +yoeman gulp

---
+++ client/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('creel')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
