6d9cff0fe8aa0eda5334af715028607dd0b71c34
Tyler Smith <tyler.e.smith000@gmail.com>
2016-08-20 21:17:48 +0000

    added ngResources

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,6 @@
 //= require angular/angular
 //= require angular-route/angular-route
 //= require angular-rails-templates
-//= requrie angular-resource/angular-resource
+//= require angular-resource/angular-resource
 //= require_tree .
 
