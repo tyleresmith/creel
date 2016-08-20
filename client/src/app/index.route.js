fc36ad14f7e3f2e50d8356567c47452bc9fbb510
Tyler Smith <tyler.e.smith000@gmail.com>
2016-08-20 17:21:06 +0000

    mod route

--- client/src/app/index.route.js
+++ client/src/app/index.route.js
@@ -1,9 +1,28 @@
+
 (function() {
   'use strict';
 
   angular
     .module('creel')
-    .config(routerConfig);
+    .config(routerConfig)
+    .factory("Fish", function(RailsResource) {
+      var Fish;
+      return Fish = (function(superClass) {
+        extend(Fish, superClass);
+    
+        function fish() {
+          return fish.__super__.constructor.apply(this, arguments);
+        }
+    
+        fish.configure({
+          url: "/api/fish",
+          name: "fish"
+        });
+    
+        return Fish;
+
+      })(RailsResource);
+    });
 
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
@@ -16,7 +35,7 @@
       })
       .state('fish', {
         url: "/fish",
-        templateUrl: 'app/views/fish.jade',
+        templateUrl: 'app/views/fish.html',
         controller: 'FishController',
         controllerAs: 'fish'
       });
