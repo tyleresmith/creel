4ea462971958038e0803a26ac99b31fc0650af44
Tyler Smith <tyler.e.smith000@gmail.com>
2016-08-20 16:13:30 +0000

    + fish route

--- client/src/app/index.route.js
+++ client/src/app/index.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('fish', {
+        url: "/fish",
+        templateUrl: 'app/views/fish.jade',
+        controller: 'FishController',
+        controllerAs: 'fish'
       });
 
     $urlRouterProvider.otherwise('/');
