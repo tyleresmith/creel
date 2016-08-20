3cfe33fe0d4ef69f3d3cec93a739bec31db8c483
Tyler Smith <tyler.e.smith000@gmail.com>
2016-08-20 17:28:50 +0000

    remove gulp angular plumbing

--- client/src/app/controllers/fish.controller.coffee
+++
@@ -1,3 +0,0 @@
-angular.module "creel"
-  .controller "FishController", ($scope, Fish) ->
-    Fish.query().then (fish) -> $scope.fish = fish
--- client/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('creel', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
