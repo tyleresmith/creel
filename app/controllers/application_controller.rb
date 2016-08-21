3e621094e1c37f54b93df2946cc10555356a4a47
Tyler Smith <tyler.e.smith000@gmail.com>
2016-08-21 14:32:14 +0000

    resuce from no record

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
+  rescue_from ActiveRecord::RecordNotFound do
+    respond_to do |type|
+      type.all { render nothing: true, status: 404 }
+    end
+  end
 end
