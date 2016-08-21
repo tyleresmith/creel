09157b917e9c0bafc669eeb9ce2df4111fcf6547
Tyler Smith <tyler.e.smith000@gmail.com>
2016-08-21 13:31:28 +0000

    search now funcitoning

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,6 +1,12 @@
 class FishController < ApplicationController
     def index
-        @fish = Fish.all
+        @fish = if params[:keywords]
+                     Fish.where('common_name LIKE ? OR species_name LIKE ?',"%#{params[:keywords]}%","%#{params[:keywords]}%")
+                   else
+                     []
+                   end
+        
+        # @fish = Fish.all
         respond_to do |format|
           format.json { render json: @fish}
         end
