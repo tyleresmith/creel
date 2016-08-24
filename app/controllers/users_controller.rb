2ae7a4fa66dc127ddc7de623b8318eebd5f89f32
Tyler Smith <oats000@hotmail.com>
2016-08-24 01:09:30 +0000

    +index route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
-class FishController < ApplicationController
+class UsersController < ApplicationController
     #allows multiple POSTs to app
     skip_before_filter :verify_authenticity_token 
-
-    def show
+    
+    def index
         @user = current_user
+        respond_to do |format|
+          format.json { render json: @user}
+        end
+    end
+    
+    def show
+        @user = User.find(params[:id])
         respond_to do |format|
           format.json { render json: @user}
         end
