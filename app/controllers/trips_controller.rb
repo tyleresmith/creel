31eea7ae9464e2edaaf4fca392f278931c5d87fc
Tyler Smith <oats000@hotmail.com>
2016-08-25 03:04:11 +0000

    + index and show

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,20 @@
+class TripsController < ApplicationController
+    #allows multiple POSTs to app
+    skip_before_filter :verify_authenticity_token 
+    
+    def index
+        @trips = current_user.trips
+        
+        respond_to do |format|
+          format.json { render json: @trips}
+        end
+    end
+    
+    def show
+        @trip = Trip.find(params[:id])
+        respond_to do |format|
+          format.json { render json: @trip}
+        end
+    end
+
+end
