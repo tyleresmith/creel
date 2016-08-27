73d3b3d8f13ac70090d734df83409a03917c3474
Tyler Smith <oats000@hotmail.com>
2016-08-27 21:24:28 +0000

    finished methods

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,7 @@
 class TripsController < ApplicationController
     #allows multiple POSTs to app
     skip_before_filter :verify_authenticity_token 
+    before_action :set_trip!, only: [:show, :update, :destroy]
     
     def index
         @trips = current_user.trips
@@ -10,10 +11,37 @@ class TripsController < ApplicationController
     end
     
     def show
-        @trip = Trip.find(params[:id])
         respond_to do |format|
           format.json { render json: @trip}
         end
+    end
+    
+    def create
+        binding.pry
+        @trip = Trip.new(trip_params)
+        @trip.user = current_user
+        @trip.save
+        render 'show', status: 201
+    end
+    
+    def update 
+        @trip.update_attributes(trip_params)
+        head :no_content
+    end
+    
+    def destroy
+        @trip.destroy
+        head :no_content
+    end
+    
+    private
+    
+    def set_trip!
+        @trip = Trip.find(params[:id])
+    end
+    
+    def trip_params
+        params.require(:trip).permit(:location, :city, :state, :water_type, :body_type)
     end
 
 end
