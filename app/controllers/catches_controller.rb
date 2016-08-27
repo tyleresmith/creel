af44c102667dd4c5681e04a7856aff1a9423d66a
Tyler Smith <oats000@hotmail.com>
2016-08-27 19:09:13 +0000

    added in methods, copied from fish

--- app/controllers/catches_controller.rb
+++ app/controllers/catches_controller.rb
@@ -1,6 +1,7 @@
 class CatchesController < ApplicationController
     #allows multiple POSTs to app
     skip_before_filter :verify_authenticity_token 
+    before_action :set_catch!, only: [:show, :update, :destroy]
     
     def index
         @catches = current_user.catches
@@ -10,10 +11,36 @@ class CatchesController < ApplicationController
     end
     
     def show
-        @catch = Catch.find(params[:id])
         respond_to do |format|
           format.json { render json: @catch}
         end
+    end
+    
+    def create
+        @catch = Catch.new(catch_params)
+        @catch.trip = Trip.find(params[:trip_id])
+        @catch.save
+        render 'show', status: 201
+    end
+    
+    def update 
+        @catch.update_attributes(catch_params)
+        head :no_content
+    end
+    
+    def destroy
+        @catch.destroy
+        head :no_content
+    end
+    
+    private
+    
+    def set_catch!
+        @catch = Catch.find(params[:id])
+    end
+    
+    def catch_params
+        params.require(:catch).permit(:fish_id,:location, :weight, :length, :tackle)
     end
 
 end
