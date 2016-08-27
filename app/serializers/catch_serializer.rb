5018a14f57faf992d642333667fb5c15215447e1
Tyler Smith <oats000@hotmail.com>
2016-08-27 20:00:00 +0000

    +fish_id to json

--- app/serializers/catch_serializer.rb
+++ app/serializers/catch_serializer.rb
@@ -1,5 +1,5 @@
 class CatchSerializer < ActiveModel::Serializer
-  attributes :id, :length, :weight, :tackle, :location, :fish, :trip
+  attributes :id, :length, :weight, :tackle, :location, :fish, :trip, :fish_id
   has_one :fish, serializer: CatchFishSerializer
   has_one :trip, serializer: CatchTripSerializer
 end
